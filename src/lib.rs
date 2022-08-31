1452ddcc442e26056143c06dc26a00642552db7d
Jesper Bisgaard <jesperjb@gmail.com>
2022-08-31 15:50:24 +0200

    Check if position exists.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use random::random_number;
 
 type Position = (usize, usize);
 
+#[derive(Debug)]
 struct MineSweeper {
     width: usize,
     height: usize,
@@ -22,12 +23,27 @@ impl MineSweeper {
                 let mut mines:Vec<Position> = Vec::new();
 
                 while mines.len() < mine_count {
-                    mines.push((random_number(width), random_number(height)))
+                    let new_mine = (random_number(width), random_number(height));
+                    if !mines.contains(&new_mine) {
+                        mines.push((random_number(width), random_number(height)))
+                    }
                 }
 
                 mines
             },
             flagged_cells: Vec::new(),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::MineSweeper;
+
+    #[test]
+    fn test_new_game() {
+        let mine_sweeper = MineSweeper::new(10, 10, 5);
+
+        println!("{:?}", mine_sweeper);
     }
 }
