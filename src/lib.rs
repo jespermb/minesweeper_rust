162510fcb10ce82d7da3dfcb8b45af41fd4bb860
Jesper Bisgaard <jesperjb@gmail.com>
2022-09-06 12:59:45 +0200

    Added flagged cells.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
 mod random;
 
+use std::ops::Index;
+
 use random::random_number;
 
 type Position = (usize, usize);
+
+enum OpenResult {
+    Mine,
+    NoMine(u8),
+    Flagged,
+}
 
 #[derive(Debug)]
 struct MineSweeper {
@@ -32,6 +40,48 @@ impl MineSweeper {
                 mines
             },
             flagged_cells: Vec::new(),
+        }
+    }
+
+    pub fn open_cell(&mut self, position: Position) -> OpenResult{
+        if self.flagged_cells.contains(&position) {
+            return OpenResult::Flagged;
+        }
+        self.open_cells.push(position);
+
+        let is_mine = self.mines.contains(&position);
+        if is_mine {
+            OpenResult::Mine
+        } else {
+            OpenResult::NoMine(8)
+        }
+    }
+
+    pub fn neighbors(&self, (x, y): Position) -> impl Iterator<Item = Position> {
+        let width = self.width;
+        let height = self.height;
+
+        (x.min(1) - 1..=(x + 1).max(width - 1)).flat_map(move |i| (
+                y.min(1) - 1..=(y + 1).max(height - 1)).map(move |j| (i, j)
+        )).filter(move |&pos| pos != (x, y))
+    }
+
+    pub fn neighbor_mines(&self, pos: Position) -> u8 {
+        self
+            .neighbors(pos)
+            .filter(|pos| self.mines.contains(pos))
+            .count() as u8
+    }
+
+    pub fn toggle_flag(&mut self, pos: Position) {
+        if self.open_cells.contains(&pos) {
+            return;
+        }
+        if self.flagged_cells.contains(&pos) {
+            let index = self.flagged_cells.iter().position(|&item| item == pos).unwrap();
+            self.flagged_cells.remove(index);
+        } else {
+            self.flagged_cells.push(pos);
         }
     }
 }
